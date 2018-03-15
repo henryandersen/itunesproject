96814ae85caa281de9bff6528628db4dcb3f47ba
Henry Andersen <HenryAndersen@students.berkeley.net>
2018-03-15 09:33:54 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    $('#button').on('click',function () {
+        var rows = $('#results').val();
+        var artist = $('#artists').val();
+        console.log(artist);
+        $.ajax({
+            url: "https://itunes.apple.com/search?term=" + artist,
+            type: 'GET',
+            crossDomain: true,
+            dataType: 'jsonp',
+            success: function(result) {
+                console.log(result);
+                myfunction(result,rows);
+            },
+            error: function() { alert('Failed!'); }
+        });
+
+    });
+
+});
+
+
+
+
+function myfunction(json,row) {
+    console.log(json.results[0].trackName);
+    $("#table").empty();
+    var html = "<table class='table table-bordered' cellpadding='15'>";
+    for(var i = 0; i < row; i++){
+        html += "<tr><td><img src=" + json.results[i].artworkUrl60 + "></td>";
+        html += "<td>" + json.results[i].trackName +  "</td>";
+        html += "<td>" + "<audio controls='true' src='" + json.results[i].previewUrl + "' id='audio' type='audio/m4a'></audio>" + "</td>";
+        html += "</tr>"
+    }
+    html +="</table>";
+
+    console.log(html);
+    $("#table").append(html);
+
+
+}
